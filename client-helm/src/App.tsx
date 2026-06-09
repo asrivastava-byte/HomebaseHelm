ee6c0cb6120ff16d559974297b886ee1cb2063a1
Anumita Srivastava <asrivastava@joinhomebase.com>
2026-06-09 15:25:39 -0400

    feat(helm-client): mount /companies routes + nav button

--- client-helm/src/App.tsx
+++ client-helm/src/App.tsx
@@ -4,6 +4,8 @@ import { PermissionProvider, useSession } from "./lib/permissions";
 import { RoleSwitcher } from "./components/RoleSwitcher";
 import { UserLookupIndexPage } from "./pages/UserLookup/IndexPage";
 import { UserLookupShowPage } from "./pages/UserLookup/ShowPage";
+import { CompanyMerchantIndexPage } from "./pages/CompanyMerchant/IndexPage";
+import { CompanyMerchantShowPage } from "./pages/CompanyMerchant/ShowPage";
 
 function Header() {
   const { role } = useSession();
@@ -11,7 +13,8 @@ function Header() {
     <Stack direction="row" justifyContent="space-between" alignItems="center" p={2} borderBottom="1px solid #eee">
       <Stack direction="row" spacing={3} alignItems="center">
         <Typography variant="h5">Helm</Typography>
-        <Button component={RouterLink} to="/users" size="small">User lookup</Button>
+        <Button component={RouterLink} to="/users"     size="small">User lookup</Button>
+        <Button component={RouterLink} to="/companies" size="small">Company / Merchant</Button>
       </Stack>
       <Stack direction="row" spacing={2} alignItems="center">
         <Typography color="text.secondary">role: {role}</Typography>
@@ -29,8 +32,10 @@ export default function App() {
         <Box p={4}>
           <Routes>
             <Route path="/" element={<Navigate to="/users" replace />} />
-            <Route path="/users"        element={<UserLookupIndexPage />} />
-            <Route path="/users/:id"    element={<UserLookupShowPage />} />
+            <Route path="/users"          element={<UserLookupIndexPage />} />
+            <Route path="/users/:id"      element={<UserLookupShowPage />} />
+            <Route path="/companies"      element={<CompanyMerchantIndexPage />} />
+            <Route path="/companies/:id"  element={<CompanyMerchantShowPage />} />
           </Routes>
         </Box>
       </Box>
